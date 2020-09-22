687d6058f417727c169fdeb8083ae37bf7dc2e27
Athos Damiani <athos.damiani@gmail.com>
2020-09-22 15:21:20 -0300

    tidy_audio_dir e tydi_audio

--- R/data_prep.R
+++ R/data_prep.R
@@ -12,6 +12,8 @@
 #' @param dest_dir optional atomic character indicating the path where to store 
 #' the files. Default is a tempdir(). Set NULL if you want the dest dir be the
 #' same as origin (warning: it will overwrite the original wave files!)
+#' @param sample_rate integer. Sample rate.
+#' @param bits integer. 
 #'
 #' @return a character vector of the same size of audio_files with the file names of the transformed wavs.
 #'
@@ -23,7 +25,7 @@
 #' mp3_files <- list.files(mp3_dir, full.names = TRUE)
 #' 
 #' wav_files <- tidy_audio(mp3_files)
-tidy_audio <- function(audio_files, dest_dir = tempdir(), sample_rate = 16000, bits = 16, parallel = 1) {
+tidy_audio_dir <- function(audio_files, dest_dir = tempdir(), sample_rate = 16000, bits = 16, parallel = 1) {
   # if dir is set, make sure it exists
   if(!is.null(dest_dir)) {
     dest_dir <- paste0(dest_dir, "/")
@@ -55,24 +57,7 @@ tidy_audio <- function(audio_files, dest_dir = tempdir(), sample_rate = 16000, b
     # update name for the right dest_dir to store
     if(!is.null(dest_dir)) new_audio_file <- gsub("^.*/", dest_dir, new_audio_file)
     
-    # normalize
-    wav <- tuneR::normalize(wav, unit = as.character(bits))
-    
-    # downsample
-    if(wav@samp.rate > sample_rate) {
-      wav <- tuneR::downsample(wav, sample_rate)
-    }
-    
-    wav@left <- as.numeric(wav@left) %>% tidyr::replace_na(0)
-    wav@right <- as.numeric(wav@right) %>% tidyr::replace_na(0)
-    
-    if(wav@stereo) wav <- wav %>% tuneR::mono(which = "both")
-    
-    wav@left <- as.integer(wav@left)
-    wav@right <- as.integer(wav@right)
-    
-    # to mono
-    wav <- tuneR::mono(wav)
+    wav <- tidy_audio(wav, sample_rate = sample_rate, bits = bits)
     
     # write wave in the disk
     tuneR::writeWave(wav, new_audio_file)
@@ -81,5 +66,58 @@ tidy_audio <- function(audio_files, dest_dir = tempdir(), sample_rate = 16000, b
   return(new_audio_files)
 }
 
+#' Tidy MP3 or Wave
+#'
+#' Apply transformations on an wav object for modelling purposes.
+#' 
+#' The transformations are:
+#' 
+#' - mp3 to wav
+#' - normalization
+#' - stereo to mono
+#' 
+#' @param wav Wave object. Usualy returned by \code{\link[tuneR]{readMP3}}/\code{\link[tuneR]{readWave}}.
+#'
+#' @return a transformed Wave object.
+#'
+#' @export
+#'
+#' @examples
+#' 
+#' library(tuneR)
+#' mp3_dir <- system.file("mp3_sample", package = "mestrado")
+#' mp3_files <- list.files(mp3_dir, full.names = TRUE)
+#' wav <- readMP3(mp3_files[1])
+#' 
+#' transformed_wav <- tidy_audio(wav)
+tidy_audio <- function(wav, sample_rate = 16000, bits = 16) {
+  # normalize
+  wav <- tuneR::normalize(wav, unit = as.character(bits))
+  
+  # downsample
+  if(wav@samp.rate > sample_rate) {
+    wav <- tuneR::downsample(wav, sample_rate)
+  }
+  
+  wav@left <- as.numeric(wav@left) %>% tidyr::replace_na(0)
+  wav@right <- as.numeric(wav@right) %>% tidyr::replace_na(0)
+  
+  if(wav@stereo) wav <- wav %>% tuneR::mono(which = "both")
+  
+  wav@left <- as.integer(wav@left)
+  wav@right <- as.integer(wav@right)
+  
+  # to mono
+  wav <- tuneR::mono(wav)
+  
+  wav
+}
+
+
+#' see this issue https://github.com/rstudio/tensorflow/issues/402
+#' sudo apt install sox
+identity_resampling <- function(orig, dest = orig) {
+  system(glue::glue("sox {orig} -r 16000 {dest}"))
+}
 
 
--- tests/testthat/test-dataprep.R
+++ tests/testthat/test-dataprep.R
@@ -8,8 +8,8 @@ mp3_files <- list.files(mp3_dir, full.names = TRUE)
 temp_dir <- tempdir()
 temp_dir <- gsub("\\\\", "/", temp_dir) # fix windows '\\' issue
 
-test_that("tidy_audio works", {
-  wavs <- tidy_audio(mp3_files, temp_dir)
+test_that("tidy_audio_dir works", {
+  wavs <- tidy_audio_dir(mp3_files, temp_dir)
   new_audio_file <- gsub("mp3$", "wav", mp3_files)
   new_audio_file <- gsub("^.+/", paste0(temp_dir, "/"), new_audio_file)
   expect_equal(new_audio_file, wavs)
